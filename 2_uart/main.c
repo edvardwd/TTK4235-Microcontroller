1d0c5559f763be06cf762efcbb352c6a0c8a9566
Edvard Dingsør <edvard.wdinsgor@gmail.com>
2025-04-04 16:06:05 +0200

    Unpacked lab folder

---
+++ 2_uart/main.c
@@ -0,0 +1,92 @@
+#include "uart.h"
+#include "gpio.h"
+#include <stdio.h>
+#include <sys/types.h>
+
+
+ssize_t _write(int fd, const void *buf, size_t count){
+    char * letter = (char *)(buf);
+    for(int i = 0; i < count; i++){
+    uart_send(*letter);
+    letter++;
+    }
+    return count;
+}
+
+ssize_t _read(int fd, void *buf, size_t count){
+    char *str = (char *)(buf);
+    char letter;
+    do {
+        letter = uart_read();
+    } while(letter == 0);
+    *str = letter;
+    return 1;
+}
+
+void button_init(){ 
+	GPIO->PIN_CNF[13] = (3 << 2);
+	GPIO->PIN_CNF[14] = (3 << 2);
+    GPIO->PIN_CNF[15] = (3 << 2);
+    GPIO->PIN_CNF[16] = (3 << 2);
+}
+
+int buttonPressed(int button){
+	return !(GPIO->IN & (1 << (button + 12)));
+}
+
+void triggerLEDMatrix(int on){
+	for(int i = 17; i <= 20; i++){
+		if (on) GPIO->OUTCLR = (1 << i); //matrix is active-low
+		else GPIO->OUTSET = (1 << i);
+	}
+}
+
+int main(){
+    int LEDon = 0; //flag to indicate if the LED matrix is on
+    for(int i = 17; i <= 20; i++){
+		GPIO->DIRSET = (1 << i);
+		GPIO->OUTCLR = (1 << i);
+        LEDon = 1;
+	}
+
+    uart_init();
+    button_init();
+
+    int sleep;
+    int a, b;
+    
+
+    while (1){
+        //Send functionality
+        if (buttonPressed(1)){
+            uart_send('A');
+        }
+        if (buttonPressed(2)){
+            uart_send('B');
+        }
+
+        if (buttonPressed(3)){
+            iprintf("The average grade in TTK%d in %d was: %c\n\r", 4235, 2022, 'B');
+        }
+
+        if (buttonPressed(4)){
+            iprintf("Enter one integer:\n\r");
+            scanf("%d", &a);
+            iprintf("Enter another:\n\r");
+            scanf("%d", &b);
+            iprintf("%d * %d = %d\n\r", a, b, a*b);
+        }
+
+        //Receive functionality
+        if (uart_read()){
+            triggerLEDMatrix(!LEDon);
+            LEDon = !LEDon;
+        }
+
+        sleep = 1000000;
+		while(--sleep); // Delay
+    }
+
+
+    return 0;
+}
---
+++ 2_uart/uart.c
@@ -0,0 +1,83 @@
+#include "uart.h"
+#include "gpio.h"
+
+#define TXD_PIN 6
+#define RXD_PIN 8
+
+#define UART ((NRF_UART_REG*)0x40002000)
+
+typedef struct{
+    volatile uint32_t TASKS_STARTRX;
+    volatile uint32_t TASKS_STOPRX;
+    volatile uint32_t TASKS_STARTTX;
+    volatile uint32_t TASKS_STOPTX;
+    volatile uint32_t RESERVED0[3];
+    volatile uint32_t TASKS_SUSPEND;
+    volatile uint32_t RESERVED1[56];
+    volatile uint32_t EVENTS_CTS;
+    volatile uint32_t EVENTS_NCTS;
+    volatile uint32_t EVENTS_RXDRDY;
+    volatile uint32_t RESERVED2[4];
+    volatile uint32_t EVENTS_TXDRDY;
+    volatile uint32_t RESERVED3;
+    volatile uint32_t EVENTS_ERROR;
+    volatile uint32_t RESERVED4[7];
+    volatile uint32_t EVENTS_RXTO;
+    volatile uint32_t RESERVED5[46];
+    volatile uint32_t SHORTS;
+    volatile uint32_t RESERVED6[64];
+    volatile uint32_t INTENSET;
+    volatile uint32_t INTENCLR;
+    volatile uint32_t RESERVED7[93];
+    volatile uint32_t ERRORSRC;
+    volatile uint32_t RESERVED8[31];
+    volatile uint32_t ENABLE;
+    volatile uint32_t RESERVED9;
+    volatile uint32_t PSELRTS;
+    volatile uint32_t PSELTXD;
+    volatile uint32_t PSELCTS;
+    volatile uint32_t PSELRXD;
+    volatile uint32_t RXD;
+    volatile uint32_t TXD;
+    volatile uint32_t RESERVED10;
+    volatile uint32_t BAUDRATE;
+    volatile uint32_t RESERVED11[17];
+    volatile uint32_t CONFIG;
+} NRF_UART_REG;
+
+
+
+void uart_init(){
+    GPIO->PIN_CNF[TXD_PIN] = 1; //TXD output
+    GPIO->PIN_CNF[RXD_PIN] = 0; //RXD input
+
+    UART->PSELTXD = TXD_PIN;
+    UART->PSELRXD = RXD_PIN;
+
+    UART->BAUDRATE = 0x00275000; //9600 baudrate
+
+    UART->PSELCTS = 0xFFFFFFFF; //CTS disconnected
+    UART->PSELRTS = 0xFFFFFFFF; //RTS discnnected
+
+    UART->ENABLE = 4;
+    UART->TASKS_STARTRX = 1;
+}
+
+
+void uart_send(char letter){
+    UART->TASKS_STARTTX = 1;
+    UART->TXD = letter;
+    
+    while (!UART->EVENTS_TXDRDY); //wait while transmitting
+
+    UART->TASKS_STOPTX = 1;
+    UART->EVENTS_TXDRDY = 0; //clear event
+}
+
+
+char uart_read(){
+    UART->TASKS_STARTRX = 1;
+    char letter = (UART->EVENTS_RXDRDY) ? UART->RXD : 0;
+    UART->EVENTS_RXDRDY = 0;
+    return letter;
+}
